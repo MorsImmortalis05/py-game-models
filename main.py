de07478e28c4e541ac5bf5dd716cb0034c4f7842
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-08-05 12:05:06 +0300

    fixed

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ def main() -> None:
         players = json.load(file)
     for nickname, player_dict in players.items():
         guild = None
+        race_data = player_dict["race"]
         race, created = Race.objects.get_or_create(
-            name=player_dict["race"]["name"],
-            description=player_dict["race"]["description"]
+            name=race_data["name"],
+            description=race_data["description"]
         )
-        for skill_dict in player_dict["race"]["skills"]:
+        for skill_dict in race_data["skills"]:
             skill, created = Skill.objects.get_or_create(
                 name=skill_dict["name"],
                 bonus=skill_dict["bonus"],
                 race=race
             )
         if player_dict["guild"] is not None:
+            player_guild = player_dict["guild"]
             guild, created = Guild.objects.get_or_create(
-                name=player_dict["guild"]["name"],
-                description=player_dict["guild"]["description"]
+                name=player_guild["name"],
+                description=player_guild["description"]
             )
             guild = guild
         player, created = Player.objects.get_or_create(
